cac6af84f20ee7a53d709da7720055762bd55bc5
ΠΜ <pmavros@users.noreply.github.com>
2022-04-22 12:42:23 +0800

    Delete online_study-analysis.R

--- online_study-analysis.R
+++
@@ -1,9 +0,0 @@
-
-
-library(tidyverse)
-
-files <- list.files(path = "~/Downloads/", pattern = "nice-online-experiment-working",
-                    full.names = T)
-files
-survey <- map_df("/Users/panosmavros/Downloads//nice-online-experiment-working.csv", read_csv, .id = "source",  show_col_types = FALSE)
-survey
